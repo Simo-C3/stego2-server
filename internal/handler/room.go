7bcf17dbd9add6a26b409ed16e93113a1241a18a
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2024-06-15 00:32:13 +0900

    chore: Add room handler and router for rooms API

---
+++ internal/handler/room.go
@@ -0,0 +1,93 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Simo-C3/stego2-server/internal/schema"
+	"github.com/labstack/echo/v4"
+)
+
+type RoomHandler struct {
+
+}
+
+func NewRoomHandler() *RoomHandler {
+	return &RoomHandler{}
+}
+
+// @Summary Get rooms
+// @Description Get rooms
+// @Tags rooms
+// @Accept json
+// @Produce json
+// @Success 200 {object} schema.GetRoomsResponse
+// @Failure 400 {object} schema.ErrResponse
+// @Router /rooms [get]
+func (h *RoomHandler) GetRooms(c echo.Context) error {
+	rooms := []schema.Room{
+		{
+			Id: "1",
+			Name: "room1",
+			HostName: "host1",
+			MinUserNum: 1,
+			MaxUserNum: 4,
+			UseCpu: true,
+		},
+		{
+			Id: "2",
+			Name: "room2",
+			HostName: "host2",
+			MinUserNum: 2,
+			MaxUserNum: 4,
+			UseCpu: false,
+		},
+	}
+
+	mockResponse := &schema.GetRoomsResponse{
+		Rooms: rooms,
+		Total: 2,
+	}
+
+	return c.JSON(http.StatusOK, mockResponse)
+}
+
+// @Summary Create room
+// @Description Create room
+// @Tags rooms
+// @Accept json
+// @Produce json
+// @Param request body schema.CreateRoomRequest true "Create room request"
+// @Success 200 {object} schema.CreateRoomResponse
+// @Failure 400 {object} schema.ErrResponse
+// @Router /rooms/{room_id} [post]
+func (h *RoomHandler) CreateRoom(c echo.Context) error {
+
+	req := new(schema.CreateRoomRequest)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+
+	mockResponse := &schema.CreateRoomResponse{
+		RoomId: "1",
+	}
+
+	return c.JSON(http.StatusOK, mockResponse)
+}
+
+// @Summary Room matching
+// @Description Room matching
+// @Tags rooms
+// @Accept json
+// @Produce json
+// @Success 200 {object} schema.MatchingResponse
+// @Failure 400 {object} schema.ErrResponse
+// @Router /rooms/matching [get]
+func (h *RoomHandler) Matching(c echo.Context) error {
+
+	mockResponse := &schema.MatchingResponse{
+		Id: "1",
+	}
+
+	return c.JSON(http.StatusOK, mockResponse)
+}
+
---
+++ internal/router/rooms.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"github.com/Simo-C3/stego2-server/internal/handler"
+)
+
+func InitRoomRouter(g *echo.Group, roomHandler handler.RoomHandler) {
+	room := g.Group("/rooms")
+	room.GET("", roomHandler.GetRooms)
+	room.POST("", roomHandler.CreateRoom)
+	room.GET("/matching", roomHandler.Matching)
+}
