d83dcc46ffd4256f44e23c506a685d9aa4ad9376
yamato0211 <kumayama0211@gmail.com>
2024-06-15 16:38:46 +0900

    test: fix publiser

--- cmd/main.go
+++ cmd/main.go
@@ -65,7 +65,7 @@ func main() {
 	debugHandler := handler.NewDebugHandler(publisher)
 
 	// debug publisher
-	e.GET("/debug/publish", debugHandler.Publish)
+	e.POST("/debug/publish", debugHandler.Publish)
 
 	// start subscriber
 	go wsHandler.SubscribeHandle(context.Background(), "game")
