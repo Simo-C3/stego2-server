baf7d3484493bc7b764b18acbb8975da775b0797
Kurichi <yuuyapp080@gmail.com>
2024-06-16 11:01:17 +0900

    feat: 排他制御

--- internal/domain/repository/game_repo.go
+++ internal/domain/repository/game_repo.go
@@ -13,4 +13,6 @@ type GameRepository interface {
 	UpdateUser(ctx context.Context, user *model.User) error
 	DeleteGame(ctx context.Context, id string) error
 	DeleteUser(ctx context.Context, id string) error
+	EditGame(ctx context.Context, gameID string, fn func(*model.Game) error) error
+	EditUser(ctx context.Context, userID string, fn func(*model.User) error) error
 }
